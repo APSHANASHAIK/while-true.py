adecef8f22a2d7194193d7a239134e12251f1a9c
Apshana <90459816+APSHANASHAIK@users.noreply.github.com>
2023-02-24 22:01:06 +0530

    Add files via upload

---
+++ while true statement.py
@@ -0,0 +1,9 @@
+while True:
+    number=int(input("enter the value grater than 100"))
+    if number<100:
+        print("try again")  
+     
+        continue   
+    elif number>100:
+        print("congratulation you are won the match")    
+    break
